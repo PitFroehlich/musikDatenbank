5c2c34c5fadddb5d80716f3bf9fa53ce0d5214cd
p.froehlich <p.froehlich@finatix.de>
2023-06-01 16:02:51 +0200

    added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-web-services")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
---
+++ src/main/resources/db/migration/V__1_first_migration.sql
@@ -0,0 +1,4 @@
+create table Mood (
+                        ID int not null,
+                        NAME varchar(100) not null,
+);
