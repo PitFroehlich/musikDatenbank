b4970bcf0df3a1dba573dadfe81ecdfd87280945
p.froehlich <p.froehlich@finatix.de>
2023-06-01 15:25:53 +0200

    Initial commit

---
+++ src/main/kotlin/com/htwk/musikdatenbank/MusikDatenbankApplication.kt
@@ -0,0 +1,11 @@
+package com.htwk.musikdatenbank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusikDatenbankApplication
+
+fun main(args: Array<String>) {
+    runApplication<MusikDatenbankApplication>(*args)
+}
---
+++ src/test/kotlin/com/htwk/musikdatenbank/MusikDatenbankApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.htwk.musikdatenbank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusikDatenbankApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
