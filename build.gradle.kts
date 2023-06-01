5640e16fab0997da3c2f8fbd5d7c2b8f4ba20d74
p.froehlich <p.froehlich@finatix.de>
2023-06-01 17:29:13 +0200

    removed flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.1.0"
     id("io.spring.dependency-management") version "1.1.0"
-    id("org.flywaydb.flyway") version "8.5.11"
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
     kotlin("plugin.jpa") version "1.8.21"
@@ -23,19 +22,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web-services")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.flywaydb:flyway-core")
-    implementation("org.flywaydb:flyway-mysql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-flyway {
-    driver = "com.mysql.jdbc.Driver"
-    url = "jdbc:mysql://localhost:3307/db"
-    user = "root"
-    password = "letmein"
-}
+
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
