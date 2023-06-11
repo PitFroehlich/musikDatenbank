127d759b32144eda90ccba37bac96ef5259e3d1d
Cati <c.thimm@finatix.de>
2023-06-11 14:10:53 +0200

    Add @Named Custom Methods in order to Map a ByteArray to a Spring Resource

--- src/main/kotlin/com/htwk/musikdatenbank/entities/publicplaylist/PublicPlaylist.kt
+++ src/main/kotlin/com/htwk/musikdatenbank/entities/publicplaylist/PublicPlaylist.kt
@@ -1,6 +1,7 @@
 package com.htwk.musikdatenbank.entities.publicplaylist
 
 import jakarta.persistence.*
+import org.springframework.core.io.Resource
 import java.sql.Blob
 
 @Entity
@@ -10,8 +11,7 @@ class PublicPlaylist (
     @Column(name = "id", updatable = false, nullable = false)
     val id: Long,
     var name: String,
-    var test: String,
-    @Lob
-    var previewPicture: Blob,
+    var text: String,
+    var previewPicture: ByteArray,
     var visible: Boolean
 )
--- src/main/kotlin/com/htwk/musikdatenbank/entities/publicplaylist/PublicPlaylistConverter.kt
+++ src/main/kotlin/com/htwk/musikdatenbank/entities/publicplaylist/PublicPlaylistConverter.kt
@@ -4,14 +4,21 @@ import com.htwk.musikdatenbank.helpers.Converter
 import org.springframework.core.io.Resource
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import org.mapstruct.Named
 import org.openapitools.model.PublicPlaylistView
-import java.sql.Blob
+import org.springframework.core.io.ByteArrayResource
 
+// NOTE: There is an issue with Kotlin and interfaces and the @Named annotation, that's why we use abstract classes here
 @Mapper
-interface PublicPlaylistConverter: Converter<PublicPlaylistView, PublicPlaylist> {
+abstract class PublicPlaylistConverter : Converter<PublicPlaylistView, PublicPlaylist> {
+    @Mapping(source = "previewPicture", target = "previewPicture", qualifiedByName = ["ByteToResource"])
+    abstract override fun convertToView(entity: PublicPlaylist): PublicPlaylistView
 
+    @Mapping(source = "previewPicture", target = "previewPicture", qualifiedByName = ["ResourceToByte"])
+    abstract override fun convertToEntity(view: PublicPlaylistView): PublicPlaylist
+    @Named("ResourceToByte")
+    fun resourceToByte(resource: Resource): ByteArray = resource.contentAsByteArray
 
-    override fun convertToView(entity: PublicPlaylist): PublicPlaylistView
-
-    fun map(value: Resource): Blob
+    @Named("ByteToResource")
+    fun byteToResource(bytes: ByteArray): Resource = ByteArrayResource(bytes)
 }
