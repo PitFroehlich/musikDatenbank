c0750ac81973a894e0937436b62c08893a7e782f
Cati <c.thimm@finatix.de>
2023-07-19 17:18:32 +0200

    add dummy path for testing and fix type

--- src/main/kotlin/com/htwk/musikdatenbank/entities/title/TitleController.kt
+++ src/main/kotlin/com/htwk/musikdatenbank/entities/title/TitleController.kt
@@ -5,6 +5,7 @@ import org.mapstruct.factory.Mappers
 import org.openapitools.api.TitleApi
 import org.openapitools.model.TitleView
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -23,6 +24,11 @@ class TitleController(
         val titles = musicService.searchTitle(keyword).map { converter.convertToView(it) }.toList()
 
         return ResponseEntity.ok(titles)
+    }
+
+    @GetMapping("/hallo")
+    fun hallo(): ResponseEntity<Unit> {
+        return ResponseEntity.status(200).build()
     }
 }
 
